f391e611ac35a39c8e24b8f0b4ecdc6e24232bd0
Sean Marcia <sean@cs.uoregon.edu>
2014-04-29 15:12:34 -0400

    Updated the controller to switch the main image to the one selected

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,10 +12,15 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
 
     $scope.orderProp = 'age';
   }]);
- 
+
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
     $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
       $scope.phone = data;
+      $scope.mainImageUrl = data.images[0];
     });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
   }]);
