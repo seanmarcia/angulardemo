3aebc7e508876e39025195a77a2523b93f2eb64c
Sean Marcia <sean@cs.uoregon.edu>
2014-04-29 14:09:37 -0400

    Updated the controllers file ot reflect that we are using routes.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,18 @@
 
 /* Controllers */
 
-var phonecatApp = angular.module('phonecatApp', []);
- 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
       $scope.phones = data;
     });
- 
+
     $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
   }]);
